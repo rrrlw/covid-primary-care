01059755110b73596c0ae5c410f621ec775e0c88
Raoul Wadhwa <raoulwadhwa@gmail.com>
2020-06-07 19:38:38 -0400

    added bubble map generation

--- generate.R
+++ generate.R
@@ -1,28 +1,59 @@
 #####SETUP#####
-library("readr")
-library("dplyr")
-library("ggplot2")
-library("usmap")
+suppressPackageStartupMessages(library("readr"))
+suppressPackageStartupMessages(library("dplyr"))
+suppressPackageStartupMessages(library("ggplot2"))
 
 #####INPUT#####
 pcare <- read_csv("aamc-state-data.csv")
+covid <- read_csv("covid-confirmed.csv")
 
 #####PROCESS#####
-# add column for # of physicians over 60 per 100,000 population
-pcare <- mutate(pcare, Risk = Number * Over60 / 100)
+# pcare:
+#   - add column for # of physicians over 60 per 100K population
+#   - only select states that can be plotted (remove DC, PR)
+pcare <- pcare %>%
+  mutate(Risk = Number * Over60 / 100) %>%
+  filter(!(state %in% c("DC", "PR")))
 
-# only select states that can be plotted (remove DC, PR)
-pcare_plot <- pcare[pcare$state != "DC" &
-                    pcare$state != "PR", ]
+# covid:
+#   - only select date column that will actually be used
+#   - rename Long_ to long (contains longitude)
+#   - rename Lat to lat (contains latitude)
+#   - remove rows w/ zero cases (won't be plotted anyway, won't cause logarithm issues)
+#   - remove rows not in continental United States
+#   - take logarithm to make bubbles more aesthetically pleasing
+covid <- covid %>%
+  select(Lat, Long_, `6/4/2020`) %>%
+  rename(long = Long_) %>%
+  rename(lat = Lat) %>%
+  filter(`6/4/2020` > 0) %>%
+  filter(lat > 25 & lat < 50 & long > -130 & long < -65) %>%
+  mutate(LogCases = log10(`6/4/2020`))
+
+# prepare map data
+states_pleth <- map_data("state")
+state_data <- left_join(states_pleth, pcare, by = "region")
 
 #####VISUALIZE & SAVE#####
-# map of 50 U.S. states
-plot_usmap(data = pcare_plot, values = "Risk", color = "black") +
-  scale_fill_gradient(name = "At-risk",
-                      low = "white", high = "black") +
-  theme(legend.position = "right")
+## bubble map of 50 U.S. states
+ggplot() +
+  # map w/ states colored by # `physicians over 60` per 100K population
+  geom_polygon(data = state_data,
+               aes(x = long, y = lat, group = group, fill = Risk),
+               color = "black") +
+  scale_fill_gradient("At-risk", low = "lightblue", high = "darkblue") +
+  # bubbles w/ log(COVID cases)
+  geom_point(data = covid,
+             aes(x = long, y = lat, size = LogCases),
+             colour = "red", alpha = 0.45, shape = 16) +
+  scale_size_continuous(range = c(0, 1.25)) +
+  # remove elements we don't need
+  theme(axis.title = element_blank(),
+        axis.text = element_blank(),
+        axis.ticks = element_blank(),
+        panel.grid = element_blank(),
+        panel.background = element_blank())
 
-# histogram of Risk column in pcare (probably won't use)
-ggplot(data = pcare, aes(x = Risk)) +
-  geom_histogram() +
-  theme_bw()
+# save bubble chloropleth
+ggsave(filename = "BubbleMap.png",
+       width = 8, height = 4)
